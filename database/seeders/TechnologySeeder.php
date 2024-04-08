1aec47bb9ac73199c58078a7f19163a6ac9c95d1
lucapolzone <lucapolzone@gmail.com>
2024-04-08 18:21:30 +0200

    feat: add Technology Model and Seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $technology_data = ['HTML', 'CSS', 'Bootstrap', 'JavaScript ES5', 'VueJS3', 'Axios', 'RESTful API', 'JSON', 'SQL', 'PHP, Laravel']; 
+        
+        foreach ($technology_data as $_technology) {
+            $technology = new Technology;
+            $technology->label = $_technology;
+            $technology->color = $faker->hexColor(); //metodo di FakerPHP
+            $technology->save();
+        }
+
+    }
+}
